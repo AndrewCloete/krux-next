e44a4886de03d5569215d8d0ff57553cf1d63f67
Andrew Cloete <andrew@x1>
2023-03-04 22:09:07 +0200

    Adds dining1 image to landing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,25 @@
-import type { ReactElement } from "react";
+import { ReactElement } from "react";
 import Layout from "../components/layout";
 import type { NextPageWithLayout } from "./_app";
+import styles from "../components/test.module.css";
 
 const Page: NextPageWithLayout = () => {
-  return <p>home</p>;
+  return (
+    <>
+      <div
+        className={
+          "h-[100vh] flex flex-col justify-center " + styles.bg_dining1
+        }
+      >
+        <div className="mx-auto flex flex-col">
+          <h1 className="text-7xl flex mx-auto text-gray-300">KRUX</h1>
+          <h1 className="text-lg font-sans text-gray-300">
+            A Community of Christian Formation
+          </h1>
+        </div>
+      </div>
+    </>
+  );
 };
 
 Page.getLayout = function getLayout(page: ReactElement) {
